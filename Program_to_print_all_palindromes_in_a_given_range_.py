0eb4fa5dc4fd53574dde77d5486df1cc2fa3ac9b
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-09-06 18:42:17 +0530

    def ispalindrome(n):     s=0     t=n     while(n!=0):         r=n%10         s=s*10+r         n//=10     if(s==t):         return True     return False a=int(input()) b=int(input()) for i in range(a,b+1):     if(ispalindrome(i)):         print(i,end=' ')

---
+++ Program_to_print_all_palindromes_in_a_given_range_.py
@@ -0,0 +1,15 @@
+def ispalindrome(n):
+    s=0
+    t=n
+    while(n!=0):
+        r=n%10
+        s=s*10+r
+        n//=10
+    if(s==t):
+        return True
+    return False
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if(ispalindrome(i)):
+        print(i,end=' ')
