3e5d3e2823f768b5e29f16b43befbc7447efb969
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-09-06 18:23:17 +0530

    import math a=int(input()) b=a*a c=int(math.log10(a)+1) f=b%int(10**c) if(f==a):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,9 +1,9 @@
 import math
 a=int(input())
-len=int(math.log10(a)+1)
-sq=int(a**2)
-c=int(sq%(10**len))
-if(c==a):
+b=a*a
+c=int(math.log10(a)+1)
+f=b%int(10**c)
+if(f==a):
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
